a94be6a39254fe79be0e6e28217e3a995cf8186a
milnus <44769523+milnus@users.noreply.github.com>
2021-10-15 12:22:57 +1100

    change import statement in check_dependencies

--- Magphi/check_depencies.py
+++ Magphi/check_depencies.py
@@ -2,7 +2,7 @@ import subprocess
 import warnings
 import sys
 # from Magphi.exit_with_error import exit_with_error
-from exit_with_error import exit_with_error
+from Magphi.exit_with_error import exit_with_error
 EXIT_DEPENDENCY_ERROR = 4
 
 
